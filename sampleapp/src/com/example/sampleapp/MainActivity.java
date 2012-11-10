41d812970f7ca99d97b25a7d19176e9c9be26abc
murali <vimurli.ace@gmail.com>
2012-11-10 18:42:12 +1100

    Initial Commit
    Signed-off-by: murali <vimurli.ace@gmail.com>

---
+++ sampleapp/src/com/example/sampleapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sampleapp;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/login.html");
+    }
+}
